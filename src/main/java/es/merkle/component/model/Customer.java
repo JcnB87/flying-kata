05c0220b50099b30a05f12fa26aa514a4a2e21e3
JoaoBorges <107804522+JcnB87@users.noreply.github.com>
2024-11-23 17:42:15 +0000

    Task nr1: adding constructor entities

--- src/main/java/es/merkle/component/model/Customer.java
+++ src/main/java/es/merkle/component/model/Customer.java
@@ -2,6 +2,8 @@ package es.merkle.component.model;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.SuperBuilder;
@@ -9,6 +11,7 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 @Data
 @RequiredArgsConstructor
+@AllArgsConstructor
 public class Customer {
     private String id;
     private String name;
--- src/main/java/es/merkle/component/model/Product.java
+++ src/main/java/es/merkle/component/model/Product.java
@@ -9,7 +9,6 @@ import java.time.LocalDate;
 @Data
 @RequiredArgsConstructor
 @AllArgsConstructor
-@NoArgsConstructor
 public class Product {
     private String id;
     private String name;
