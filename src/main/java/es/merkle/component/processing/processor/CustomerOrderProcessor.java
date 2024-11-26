62c932ee699ab27f8b66204e94099bae36616907
JoaoBorges <107804522+JcnB87@users.noreply.github.com>
2024-11-26 14:56:26 +0000

    CustomerOrderProcessor class, add remove operation implementation

--- src/main/java/es/merkle/component/processing/processor/CustomerOrderProcessor.java
+++ src/main/java/es/merkle/component/processing/processor/CustomerOrderProcessor.java
@@ -18,18 +18,32 @@ public class CustomerOrderProcessor implements OrderProcessor {
 
     @Override
     public boolean accepts(Order order) {
-        return order.getOrderType() == OrderType.ADD;
+        return order.getOrderType() == OrderType.ADD || order.getOrderType() == OrderType.REMOVE;
     }
 
     @Override
     public void process(Order order) {
-        Product productOrder = productAdapter.getProduct(order.getProcessingProductId());
-        order.getAddingProducts().add(productOrder);
+        if (order.getOrderType() == OrderType.ADD) {
+            Product productOrder = productAdapter.getProduct(order.getProcessingProductId());
+            order.getAddingProducts().add(productOrder);
 
-        BigDecimal finalPrice = order.getAddingProducts().stream()
-                .map(Product::getPrice)
-                .reduce(BigDecimal.ZERO, BigDecimal::add);
+            BigDecimal finalPrice = order.getAddingProducts().stream()
+                    .map(Product::getPrice)
+                    .reduce(BigDecimal.ZERO, BigDecimal::add);
 //                .reduce(BigDecimal.ZERO, (acc, price) -> acc.add(price));
-        order.setFinalPrice(finalPrice);
+            order.setFinalPrice(finalPrice);
+        } else if (order.getOrderType() == OrderType.REMOVE) {
+            Product productOrder = productAdapter.getProduct(order.getProcessingProductId());
+            order.getRemoveProducts().add(productOrder);
+
+            // Update AddingProducts by removing products that are in removeProducts
+            order.getAddingProducts().removeAll(order.getRemoveProducts());
+
+            BigDecimal finalPrice = order.getAddingProducts().stream()
+                    .map(Product::getPrice)
+                    .reduce(BigDecimal.ZERO, BigDecimal::add);
+            order.setFinalPrice(finalPrice);
+        }
     }
+
 }
