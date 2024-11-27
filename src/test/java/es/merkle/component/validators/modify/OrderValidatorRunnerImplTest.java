08b27a9e2af0cbce42da27dbb01449fd92337d24
JoaoBorges <107804522+JcnB87@users.noreply.github.com>
2024-11-27 14:16:32 +0000

    tidy up

--- src/test/java/es/merkle/component/validators/modify/OrderValidatorRunnerImplTest.java
+++ src/test/java/es/merkle/component/validators/modify/OrderValidatorRunnerImplTest.java
@@ -14,7 +14,7 @@ import java.util.List;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
-class CustomOrderValidatorImplTest {
+class OrderValidatorRunnerImplTest {
 
     @MockBean
     private OrderValidator orderValidator;
